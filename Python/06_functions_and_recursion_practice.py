62843a78f49ae8172744479d5059d35278cad55b
Piyaro Khan <sharpiyarokhan@gmail.com>
2026-06-03 19:49:08 -0700

    Clean up code comments and formatting

--- Python/06_functions_and_recursion_practice.py
+++ Python/06_functions_and_recursion_practice.py
@@ -7,13 +7,13 @@
 #       return value
 # Func_name(arg1,arg2)  ---->  Function Call
 
-# def calc_Sum(a,b):
-#     return a+b
-# print("The Sum is: ",calc_Sum(9,4))
+def calc_Sum(a,b):
+    return a+b
+print("The Sum is: ",calc_Sum(9,4))
 
-# def avg(p1,p2,p3):
-#     return (p1+p2+p3)/3
-# print("The avg of given nums is: ",avg(2,7,3))
+def avg(p1,p2,p3):
+    return (p1+p2+p3)/3
+print("The avg of given nums is: ",avg(2,7,3))
 
 # Functions in Python:
 # 1: Builtin Functions:
@@ -23,48 +23,48 @@
 #   Fuctions that are defined by user for its personal use are known as User Defined Fucntions
 
 # Default Arguments:
-#def calc_prod(a=1,b) // this will make through an error becuase default arg are written after non default arguments
-# def calc_prod(a=1,b=2):
-#     return a*b
-# print(calc_prod())
+# def calc_prod(a=1,b) //this will make through an error becuase default arg are written after non default arguments
+def calc_prod(a=1,b=2):
+    return a*b
+print(calc_prod())
 
 # Practice:
  
 # WAP to print the len of a list , take list as an argument
-# def calc_len(l1):
-#     return len(l1)
-# list1=[2,5,2,6,5]
-# print("The len of given list is: ",calc_len(list1))
+def calc_len(l1):
+    return len(l1)
+list1=[2,5,2,6,5]
+print("The len of given list is: ",calc_len(list1))
 
 # WAP to print the elements of a list in a single line,(list as paramenter)
-# def print_el(l1):
-#     for el in l1:
-#         print(el,end=" ")
-# list1=[2,5,2,6,5]
-# print_el(list1)
+def print_el(l1):
+    for el in l1:
+        print(el,end=" ")
+list1=[2,5,2,6,5]
+print_el(list1)
 
 # WAP to find the factorial of n numbers,(n as an arg);
-# def calc_fact(n):
-#     fact=1
-#     for el in range(1,n+1):
-#         fact*=el
-#     print("The fact of ",n," is: ",fact)
-# calc_fact(3)
+def calc_fact(n):
+    fact=1
+    for el in range(1,n+1):
+        fact*=el
+    print("The fact of ",n," is: ",fact)
+calc_fact(3)
 
 # WAP to convert USD to PKR: lets 1 USD = 86 PKR
-# def convert_cur(usd_val):
-#     pkr_val=usd_val*280
-#     print(usd_val," usd = ",pkr_val,"pkr")
-# convert_cur(2)
+def convert_cur(usd_val):
+    pkr_val=usd_val*280
+    print(usd_val," usd = ",pkr_val,"pkr")
+convert_cur(2)
 
 # HOME WORK: WAF that return odd or even value depending upon user.
-# def func(n):
-#     if(n%2==0):
-#         return "even"
-#     else:
-#         return "odd"
-# n=int(input("enter a num:"))
-# print(func(n))  
+def func(n):
+    if(n%2==0):
+        return "even"
+    else:
+        return "odd"
+n=int(input("enter a num:"))
+print(func(n))  
 
 # Recursion: When a function calls itself repeadily its known as recursion.
 
@@ -75,28 +75,28 @@ def show(n):
     show(n-1)
 show(4)
 
-# def fact(n):
-#     if(n==0 or n==1):
-#         return 1
-#     else:
-#         return n*fact(n-1)
-# print(fact(5))
+def fact(n):
+    if(n==0 or n==1):
+        return 1      # This 1 will return where from it is being called.
+    else:
+        return n*fact(n-1)
+print(fact(5))
 
 # Lets Practice:
 # 1: Write a recursive function to calculate the sum of first n natural numbers.
-# def calc_sum(n):
-#     if(n==0):
-#         return 0
-#     else:
-#         return calc_sum(n-1)+n
-# print(calc_sum(10))
+def calc_sum(n):
+    if(n==0):
+        return 0
+    else:
+        return calc_sum(n-1)+n
+print(calc_sum(10))
 
 # 2: Write a recursive function to print the elements of a list.
 # (hint: use list and indix)
-# def print_el(list,indx=0):
-#     if(indx==len(list)):
-#         return
-#     print(list[indx])
-#     print_el(list,indx+1)
-# fruits=["mango,banana,apple"]
-# print_el(fruits)
+def print_el(list,indx=0):
+    if(indx==len(list)):
+        return
+    print(list[indx])
+    print_el(list,indx+1)
+fruits=["mango,banana,apple"]
+print_el(fruits)
